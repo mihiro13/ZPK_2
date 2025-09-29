5dd93cae208b0a2408cba670581c2e554db33be5
newUserName <173579246+mihiro13@users.noreply.github.com>
2025-09-29 11:14:37 +0900

    time

--- behavior_pack/scripts/util/timeHelper.js
+++ behavior_pack/scripts/util/timeHelper.js
@@ -1,10 +1,10 @@
 export function formatDateTime(date, timezone) {
     const offset = timezone * 60 * 60 * 1000;
-    const date = new Date(date.getTime() + offset);
+    const newDate = new Date(date.getTime() + offset);
 
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+    const hours = String(newDate.getUTCHours()).padStart(2, '0');
+    const minutes = String(newDate.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(newDate.getUTCSeconds()).padStart(2, '0');
 
     return `${hours}:${minutes}:${seconds}`;
 }
